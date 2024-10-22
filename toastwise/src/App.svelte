1c9be839957e1d694aaa661334d63559323275fa
Prabhjot Singh <97115364+PrabhjotxSingh@users.noreply.github.com>
2024-10-21 22:52:09 -0400

    added background

--- toastwise/src/App.svelte
+++ toastwise/src/App.svelte
@@ -6,7 +6,9 @@
 <main>
   <div class="MainDiv">
     <div class="toaster">
-      <Toaster />
+      <div class="toaster_container">
+        <Toaster />
+      </div>
     </div>
     <div class="phone">
       <Phone />
@@ -15,7 +17,6 @@
 </main>
 
 <style>
-
   .toaster {
     width: 70%; /* Set width to 70% */
     border: 1px solid black;
@@ -24,10 +25,10 @@
   .phone {
     width: 30%; /* Set width to 30% */
     border: 1px solid black;
-
   }
 
-  .toaster, .phone{
+  .toaster,
+  .phone {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -35,9 +36,14 @@
   .MainDiv {
     display: flex;
     align-items: stretch; /* Ensure child divs stretch to full height */
-    justify-content: center; 
-    height: 100vh; 
+    justify-content: center;
+    height: 100vh;
   }
-
-
+  .toaster_container {
+    background-color: white;
+    padding: 180px;
+    background-image: url("https://st.depositphotos.com/1000674/4891/v/450/depositphotos_48910607-stock-illustration-bread-toaster.jpg");
+    background-size: cover;
+    background-position: center;
+  }
 </style>
